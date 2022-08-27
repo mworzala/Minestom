fa0a5199db91c690c6b3cebdf7f4be48fb43827c
mworzala <mattheworzala@gmail.com>
2022-08-27 20:52:30 +0300

    remove extensions

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -5,7 +5,6 @@ import net.minestom.server.adventure.bossbar.BossBarManager;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.exception.ExceptionManager;
-import net.minestom.server.extensions.ExtensionManager;
 import net.minestom.server.gamedata.tags.TagManager;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.BlockManager;
@@ -297,10 +296,6 @@ public final class MinecraftServer {
 
     public static AdvancementManager getAdvancementManager() {
         return serverProcess.advancement();
-    }
-
-    public static ExtensionManager getExtensionManager() {
-        return serverProcess.extension();
     }
 
     public static TagManager getTagManager() {
